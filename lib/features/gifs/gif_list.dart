777cc447cfac6a1e9342098229cebf768cc38ac4
ArthurRukoly <arthurrukoly000@gmail.com>
2025-02-16 02:13:05 +0200

    adapted screen for grid

--- lib/features/gifs/gif_list.dart
+++ lib/features/gifs/gif_list.dart
@@ -15,25 +15,29 @@ class GifList extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      controller: scrollController, // Attach ScrollController
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, // 2 GIFs per row
-        mainAxisSpacing: 8.0,
-        crossAxisSpacing: 8.0,
-        childAspectRatio: 1, // Keeps GIFs square
-      ),
-      padding: EdgeInsets.all(8.0),
-      itemCount: gifs.length + 1, // Add one extra for the loading indicator
-      itemBuilder: (context, index) {
-        if (index == gifs.length) {
-          return isLoadingMore ? LoadingIndicator() : SizedBox.shrink();
-        }
+Widget build(BuildContext context) {
+  return LayoutBuilder(
+    builder: (context, constraints) {
+      return GridView.builder(
+        controller: scrollController,
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200, // Each item will be at most 200px wide
+          mainAxisSpacing: 8.0,
+          crossAxisSpacing: 8.0,
+          childAspectRatio: 1, // Keeps GIFs square
+        ),
+        padding: EdgeInsets.all(8.0),
+        itemCount: gifs.length + 1,
+        itemBuilder: (context, index) {
+          if (index == gifs.length) {
+            return isLoadingMore ? LoadingIndicator() : SizedBox.shrink();
+          }
 
-        final gifData = gifs[index];
-        return GifCard(gifData: gifData);
-      },
-    );
-  }
+          final gifData = gifs[index];
+          return GifCard(gifData: gifData);
+        },
+      );
+    },
+  );
+}
 }
